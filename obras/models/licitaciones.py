b1c40bb3ad9f190eb25fa5a7597b29269797df27
CattaArriagada <CattaArriagada@github.com>
2020-12-21 01:53:20 -0300

    Agrega clase materiales y detalle presupuesto

--- obras/models/licitaciones.py
+++ obras/models/licitaciones.py
@@ -14,3 +14,37 @@ class Presupuesto(models.Model):
         #detalle_presupuesto_material_ids = fields.One2many('obras.detalle_presupuesto','presupuesto_d_id')
         #orden_ids = fields.Many2many('orden_compra.orden_compras', string="Orden de compra")
        
+class Detalle_presupuesto_materiales(models.Model):
+
+        _name = "obras.detalle_presupuesto"
+
+        name =fields.Char(string='Detalle del presupuesto')
+        
+        cost = fields.Float(string='Costo')
+        cantidad = fields.Integer(string='Cantidad solicitada')
+        sub_total = fields.Integer(string='Total', compute='_subtotal')
+
+        @api.one
+        def _subtotal(self):
+               self.sub_total=self.cost*self.cantidad
+        
+        #relaciones in
+        presupuesto_d_id = fields.Many2one('obras.presupuesto')
+        materiales_d_id = fields.Many2one('obras.materiales', string='Material')
+
+class Material(models.Model):
+        _name = 'obras.materiales'
+
+        name = fields.Char(string='Nombre Producto', required=True)
+        #codigo = fields.Integer(string='Codigo', required=True)
+        
+        #relacion in
+        detalle_presupuesto_material_ids = fields.One2many('obras.detalle_presupuesto','materiales_d_id')
+        #relacion out
+        detalle_orden_ids = fields.One2many('orden_compra.detalle_orden','materiales_o_id')
+        #estamos tocando
+        #cobros_id = fields.Many2one('avance_fisico.cobros', string='cobro')
+        #costo 
+
+# LISTO
+#otro intento
