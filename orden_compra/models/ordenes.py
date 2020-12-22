ded4708d79b5c8664f7cefddeee2b1fc6294fa43
gandalfKy <gandalfKy@github.com>
2020-12-22 11:35:52 -0300

    Crear py de orden compra

--- orden_compra/models/ordenes.py
+++ orden_compra/models/ordenes.py
@@ -1,3 +1,18 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+
+class OrdenDeCompra(models.Model):
+        _name = 'orden_compra.orden_compras'
+
+        name = fields.Char(string='N° Orden')
+       
+        licitacion_id = fields.Many2one('obras.licitaciones', string='Obra')
+        monto_total = fields.Integer(string='Monto total')
+        observacion = fields.Text(string='Observacion')
+
+        #relaciones in
+        proveedor_ids = fields.Many2one('orden_compra.proveedores',string='Proveedor asocido')
+        #relaciones out
+
+        detalle_orden_ids=fields.One2many('orden_compra.detalle_orden','orden_c_id')
