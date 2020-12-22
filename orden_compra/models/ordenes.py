a5061f6872f633679aa4388adb0fd0345e4a8743
gandalfKy <gandalfKy@github.com>
2020-12-22 11:37:55 -0300

    Crear clase detalle orden materiales de orden compra

--- orden_compra/models/ordenes.py
+++ orden_compra/models/ordenes.py
@@ -16,3 +16,21 @@ class OrdenDeCompra(models.Model):
         #relaciones out
 
         detalle_orden_ids=fields.One2many('orden_compra.detalle_orden','orden_c_id')
+
+class Detalle_Orden_Material(models.Model):
+        _name = 'orden_compra.detalle_orden'
+
+        name = fields.Char(string='Detalle orden')
+
+        cost = fields.Float(string='Costo')
+        cantidad = fields.Integer(string='Cantidad solicitada')
+        sub_total = fields.Integer(string='Total', compute='_subtotal')
+
+        #relaciones out
+        materiales_o_id = fields.Many2one('obras.materiales', string='Material')
+        #relaciones in
+        orden_c_id =fields.Many2one('orden_compra.orden_compras')
+
+        @api.one
+        def _subtotal(self):
+               self.sub_total=self.cost*self.cantidad
