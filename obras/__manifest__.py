4aae4b3347ed5619f1341d29d4bd52e79585597f
CattaArriagada <CattaArriagada@github.com>
2020-12-23 17:37:38 -0300

    actualización de información

--- obras/__manifest__.py
+++ obras/__manifest__.py
@@ -1,22 +1,21 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "obras",
+    'name': "Módulo de obra",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Licitaciones""",
 
     'description': """
-        Long description of module's purpose
+        Modulo de licitaciones que conlleva la obra
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Catalina Arriagada",
+    'website': "http://www.LaCallecita.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Insurance',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
