58c1ba548223a012b42127133c65ca8792d6584d
arkansic <arkansic@github.com>
2020-12-24 15:37:42 -0300

    correcion y formato del manifest

--- avance_fisico/__manifest__.py
+++ avance_fisico/__manifest__.py
@@ -1,22 +1,22 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "avance_fisico",
+    'name': "Modulo de Avance Fisico",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Avances y cobros
+        """,
 
     'description': """
-        Long description of module's purpose
+        Modulo que presenta un avance fisico de una obra, con sus cobros incluidos
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Victor Venegas Galaz",
+    'website': "http://www.ConstructoraLaCallecita.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Insurance',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
@@ -25,13 +25,7 @@
     # always loaded
     'data': [
         'security/ir.model.access.csv',
-        #'views/views.xml',
         'views/view_avance_licitacion.xml',
         'views/view_cobros.xml',
-        #'views/templates.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
     ],
 }
